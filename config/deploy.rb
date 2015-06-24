2b9c5bc28382b5a14e8de6ed922fc1c4e851bf8f
Brandon Brown <brandon.bbrandon@gmail.com>
2015-06-23 18:29:54 -0700

    test trying to deploy on digital ocean

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-
+require 'mina/rvm'
 # Basic settings:
 #   domain       - The hostname to SSH to.
 #   deploy_to    - Path to deploy into.
