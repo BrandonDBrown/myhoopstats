63c07692bdbff521f2fb349372a1cd8020a8c6f1
Brandon Brown <brandon.bbrandon@gmail.com>
2015-07-06 11:41:30 -0700

    do deploy 6

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ require 'mina/rvm'
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-set :term_mode, nil
+
 set :domain, 'myhoopstats.com'
 set :user, 'myhoopstats'
 set :deploy_to, '/home/myhoopstats/app'
